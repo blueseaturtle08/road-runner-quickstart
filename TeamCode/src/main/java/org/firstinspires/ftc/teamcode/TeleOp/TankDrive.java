bc36597b53f9c1925a71051d69575d8cda6c5445
MaximeSobrier <142960631+MaximeSobrier@users.noreply.github.com>
2024-08-25 16:16:01 -0700

    Update TankDrive.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TankDrive.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="TankDrive", group="TeleOp")
+public class TankDrive extends LinearOpMode {
+
+
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    @Override
+    public void runOpMode() {
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double x = -gamepad1.left_stick_y;
+            double y = -gamepad1.right_stick_y;
+
+            double leftFrontPower  = x;
+            double leftBackPower = x;
+            double rightFrontPower = y;
+            double rightBackPower  = y;
+
+            leftFrontDrive.setPower(leftFrontPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            leftBackDrive.setPower(leftBackPower);
+            rightBackDrive.setPower(rightBackPower);
+        }
+
+    }
+}
